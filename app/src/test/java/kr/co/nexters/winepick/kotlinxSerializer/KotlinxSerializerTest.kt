679dd44076f50f6b4114fbe9157a1e0a922e98be
riflockle7 <ridickle7@gmail.com>
2021-02-03 19:08:47 +0900

    [base] : 1. kotlinx.serialization 적용
    GSON 과 비슷한 직렬화/역직렬화 라이브러리

---
+++ app/src/test/java/kr/co/nexters/winepick/kotlinxSerializer/KotlinxSerializerTest.kt
@@ -0,0 +1,45 @@
+package kr.co.nexters.winepick.kotlinxSerializer
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kr.co.nexters.winepick.feature.riflockle7.DummyMovie
+import kr.co.nexters.winepick.feature.riflockle7.getDummy
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * kotlinx.serializer 를 테스트
+ *
+ * @since v1.0.0 / 2021.02.02
+ */
+class KotlinxSerializerTest {
+    @Test
+    fun jsonSerializerParseTest() {
+        // Serializing (object -> jsonString)
+        val data = getDummy()
+        val string = Json.encodeToString(data)
+        Assert.assertEquals(string, "{\"date\":\"2017-11-22\",\"grade\":15,\"id\":\"5a54be21e8a71d136fb536a1\",\"reservation_grade\":6,\"reservation_rate\":61.69,\"thumb\":\"https://raw.githubusercontent.com/riflockle7/ref/master/1.%20ImageRef/padakpadak/6.jpg\",\"title\":\"꾼\",\"user_rating\":5}")
+
+        // Deserializing (jsonString -> object)
+        val obj = Json.decodeFromString<DummyMovie>(string)
+        Assert.assertEquals(obj, getDummy())
+
+        // backingField Serializing (object -> jsonString)
+        val backingFieldData = getDummy().apply { stars = 40 }
+        Assert.assertEquals(backingFieldData.stars, 40)
+        Assert.assertEquals(backingFieldData.path, "kotlin")
+        Assert.assertEquals(backingFieldData.delegatedId, "5a54be21e8a71d136fb536a1")
+
+        // backingField Deserializing (jsonString -> object)
+        val backingFieldJsonStr = Json.encodeToString(getDummy().apply { stars = 40 })
+        Assert.assertTrue(backingFieldJsonStr.contains("stars"))
+        Assert.assertTrue(!backingFieldJsonStr.contains("path"))
+        Assert.assertTrue(!backingFieldJsonStr.contains("delegateId"))
+
+        // KSerializer 인터페이스의 인스턴스 출력하기
+        val colorSerializer: KSerializer<DummyMovie> = DummyMovie.serializer()
+        println(colorSerializer.descriptor)
+    }
+}
