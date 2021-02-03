e9cddef4c8957a011f468d76d4fd8d8662817e79
riflockle7 <ridickle7@gmail.com>
2021-02-03 21:59:31 +0900

    [base] : 3. SharedPreference
    SharedPreference 를 map 처럼 사용할 수 있도록 함

--- app/src/main/java/kr/co/nexters/winepick/WinePickApplication.kt
+++ app/src/main/java/kr/co/nexters/winepick/WinePickApplication.kt
@@ -1,6 +1,7 @@
 package kr.co.nexters.winepick
 
 import android.app.Application
+import android.content.Context
 
 /**
  * WinePick Application 클래스
@@ -9,7 +10,12 @@ import android.app.Application
  * @since v1.0.0 / 2021.01.28
  */
 class WinePickApplication : Application() {
+    companion object {
+        lateinit var appContext : Context
+    }
+
     override fun onCreate() {
         super.onCreate()
+        appContext = this
     }
 }
