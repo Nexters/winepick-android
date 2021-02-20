f19482f44e41db500388a191feea4792ca0d1506
riflockle7 <ridickle7@gmail.com>
2021-02-20 20:39:27 +0900

    [data] 상수 처리

---
+++ app/src/main/java/kr/co/nexters/winepick/constant/TestConstant.kt
@@ -0,0 +1,17 @@
+package kr.co.nexters.winepick.constant
+
+import kr.co.nexters.winepick.ui.type.TypeDetailModel
+
+/**
+ * [TypeDetailModel] 에서 사용하는 상수 모음
+ *
+ * @since v1.0.0 / 2021.02.20
+ */
+object TestConstant {
+    const val A = 1
+    const val B = 2
+    const val C = 3
+    const val D = 4
+    const val E = 5
+    const val F = 6
+}
