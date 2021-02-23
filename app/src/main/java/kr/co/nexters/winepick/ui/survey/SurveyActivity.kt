df90913ee8567f49025562fd296222d93e47a56e
anyl92 <yulim9592@gmail.com>
2021-02-24 03:44:44 +0900

    [feat] 설문 단계 이동 시 애니메이션 적용

--- app/src/main/java/kr/co/nexters/winepick/ui/survey/SurveyActivity.kt
+++ app/src/main/java/kr/co/nexters/winepick/ui/survey/SurveyActivity.kt
@@ -1,9 +1,11 @@
 package kr.co.nexters.winepick.ui.survey
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import kr.co.nexters.winepick.R
 import kr.co.nexters.winepick.data.model.Survey
 import kr.co.nexters.winepick.network.WinePickService
@@ -13,31 +15,60 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class SurveyActivity : FragmentActivity() {
+class SurveyActivity : AppCompatActivity() {
+
     var survey: Survey? = null
     var winePickService: WinePickService? = null
+
+    private var fragmentManager: FragmentManager? = null
+    private var transaction: FragmentTransaction? = null
+
+    private var surveyFragment: SurveyFragment? = null
+    private var sampleFragment: SampleFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey)
+        survey = loadSurvey()
+        Log.i("Load Survey Success", survey.toString())
 
+        fragmentManager = supportFragmentManager
+        surveyFragment = SurveyFragment()
+
+        transaction = fragmentManager!!.beginTransaction()
+        transaction!!.replace(R.id.frameLayout, surveyFragment!!).commitAllowingStateLoss()
+    }
+
+    fun btnClick(view: View) {
+        sampleFragment = SampleFragment()
+
+        transaction = fragmentManager!!.beginTransaction()
+        transaction!!
+            .setCustomAnimations(R.anim.slide_right_in, R.anim.slide_left_out,
+                R.anim.slide_left_in, R.anim.slide_right_out)
+            .replace(R.id.frameLayout, sampleFragment!!)
+            .addToBackStack(null)
+            .commitAllowingStateLoss()
+    }
+
+    fun loadSurvey(): Survey? {
         var retrofit = Retrofit.Builder()
             .baseUrl("http://ec2-3-35-107-29.ap-northeast-2.compute.amazonaws.com:8080/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         winePickService = retrofit.create(WinePickService::class.java)
-}
 
-    fun btnClick(view: View) {
         winePickService!!.getSurveys().enqueue(object: Callback<Survey> {
             override fun onFailure(call: Call<Survey>, t: Throwable) {
-                Log.e("Survey conn fail", t.message!!)
+                Log.e("Connect Survey Fail", t.message!!)
 
             }
             override fun onResponse(call: Call<Survey>, response: Response<Survey>) {
                 survey = response.body()
-                Log.i("Survey conn success", survey.toString())
+                Log.i("Connet Survey Success", survey.toString())
             }
         })
+        return survey
     }
+
 }
--- app/src/main/java/kr/co/nexters/winepick/ui/survey/SurveyFragment.java
+++ app/src/main/java/kr/co/nexters/winepick/ui/survey/SurveyFragment.java
@@ -12,7 +12,7 @@ import kr.co.nexters.winepick.R;
 
 public class SurveyFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+    /*// TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -25,14 +25,14 @@ public class SurveyFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
+    *//**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment SurveyFragment.
-     */
+     *//*
     // TODO: Rename and change types and number of parameters
     public static SurveyFragment newInstance(String param1, String param2) {
         SurveyFragment fragment = new SurveyFragment();
@@ -50,7 +50,7 @@ public class SurveyFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
+    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
