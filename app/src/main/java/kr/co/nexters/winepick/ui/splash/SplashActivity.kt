7f38583053476da61e98eb265150bd0f905a478f
yunakim2 <98_02@naver.com>
2021-02-25 00:41:06 +0900

    [feat] Splash - 1.5초로 변환

--- app/src/main/java/kr/co/nexters/winepick/ui/splash/SplashActivity.kt
+++ app/src/main/java/kr/co/nexters/winepick/ui/splash/SplashActivity.kt
@@ -57,6 +57,6 @@ class SplashActivity : AppCompatActivity() {
 //        }
     }
     companion object {
-        private const val DURATION : Long = 2000
+        private const val DURATION : Long = 1500
     }
 }
