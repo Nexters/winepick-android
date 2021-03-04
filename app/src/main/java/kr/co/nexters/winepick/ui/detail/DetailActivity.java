fe09bff4e7173a98875ac2c173879f9ae10511da
anyl92 <yulim9592@gmail.com>
2021-03-04 18:43:35 +0900

    [feat] 상세페이지 UI - 와인 설명 텍스트, 특징까지 완료

---
+++ app/src/main/java/kr/co/nexters/winepick/ui/detail/DetailActivity.java
@@ -0,0 +1,13 @@
+package kr.co.nexters.winepick.ui.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
