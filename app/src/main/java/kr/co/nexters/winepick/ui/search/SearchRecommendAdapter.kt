6107a2752024d02f92e9b936ff1cbd8e8c2a57bc
riflockle7 <ridickle7@gmail.com>
2021-04-03 13:15:52 +0900

    [bug] 검색 빠른 키워드 잘림 내용 수정

--- app/src/main/java/kr/co/nexters/winepick/ui/search/SearchRecommendAdapter.kt
+++ app/src/main/java/kr/co/nexters/winepick/ui/search/SearchRecommendAdapter.kt
@@ -123,7 +123,7 @@ class SearchRecommendAdapter(
         /* Get the ItemView and minimize it's height as small as possible
             to fit as many cells as it's possible in the screen. */
 
-        val itemView = holder.itemView.apply { layoutParams.height = 42.dpToPx() }
+        val itemView = holder.itemView
 
         val globalLayoutListener = object : OnGlobalLayoutListener {
             override fun onGlobalLayout() {
@@ -132,7 +132,7 @@ class SearchRecommendAdapter(
 
                 // RecyclerView 내에서 아이템뷰가 차지하는 % 를 구한다 (소수점 값)
                 val title = itemView.findViewById<TextView>(R.id.tv_search_recommend_title)
-                val span = (title.width.pxToDp()) / measureHelper.baseCell
+                val span = (title.width.pxToDp() + 10.dpToPx()) / measureHelper.baseCell
 
                 // 새로운 View 가 들어갈 것임을 알린다.
                 measureHelper.addMeasuredCount()
