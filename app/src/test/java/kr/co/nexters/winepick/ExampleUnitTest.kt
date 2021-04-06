1545ec102d8a0a083c3ddf6be4c4f860983cdfec
riflockle7 <ridickle7@gmail.com>
2021-04-06 13:57:42 +0900

    [feat] 스프레드시트 내 resource 등록 및 리소스 매치 테스트 코드 작성
    엑셀 링크 : https://docs.google.com/spreadsheets/d/1d5XrtbHtnGChW3Hn2fmuZK5u41z8E0RinIKXISHEv6k/edit#gid=217775320
    
    기획팀에서 정리된 내용에 따라 내용 업데이트 예정

--- app/src/test/java/kr/co/nexters/winepick/ExampleUnitTest.kt
+++ app/src/test/java/kr/co/nexters/winepick/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package kr.co.nexters.winepick
 import androidx.test.core.app.ActivityScenario
 import kr.co.nexters.winepick.base.AndroidBaseTest
 import kr.co.nexters.winepick.example.kotlin.viewmodel.KotlinViewModelActivity
+import org.junit.Assert
 import org.junit.Test
 
 
@@ -16,6 +17,39 @@ class ExampleUnitTest : AndroidBaseTest() {
             scenario.onActivity { activity: KotlinViewModelActivity ->
                 assert(activity.viewModel.title.value == "코틀린 액티비티 테스트")
             }
+        }
+    }
+
+    /** 와인 카테고리별 정리가 실제로 잘 동작하는지 테스트한다. */
+    @Test
+    fun test() {
+        // 카테고리별 데이터 모음
+        val resources = WinePickApplication.appContext?.resources
+        val meats = resources?.getStringArray(R.array.meats)
+        val italyFoods = resources?.getStringArray(R.array.italyFoods)
+        val cheeses = resources?.getStringArray(R.array.cheeses)
+
+        // 이미지 resource Id -> 각 카테고리별 데이터 모음
+        val directories = mapOf(
+            R.array.meats to meats,
+            R.array.italyFoods to italyFoods,
+            R.array.cheeses to cheeses,
+        )
+
+        // 함께하면 더 맛있는 요리 데이터 정제
+        listOf("그릴에 구운 바비큐", "훈제오리", "토마토소스 파스타", "피자", "신선한 치즈").map { keyword ->
+            val resource = directories
+                // directories 에서 해당 요리가 속하는 데이터 찾기
+                .filter { it.value?.contains(keyword) ?: false }
+                // 키값 뽑아냄
+                .map { it.key }.firstOrNull()
+
+            println("$keyword contains ${directories[resource]?.toList()}")
+            val contains = directories[resource]?.toList()?.contains(keyword) ?: false
+            if (keyword != "훈제오리" && keyword != "피자")
+                Assert.assertEquals(contains, true)
+            else
+                Assert.assertEquals(contains, false)
         }
     }
 }
